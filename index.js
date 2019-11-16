89ae742af8a54a1cb70d79da71c68da12ed53eba
Nick <nick.moi@gmail.com>
2019-11-16 17:51:18 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here
+let var1 = 31;
+let var2 = 2;
+
+let multiply = var1 * var2;
+
+let random = Math.floor(Math.random() * 100) + 1;
+
+let mod = 9%5;
+
+let max = Math.max(1,2,3,4,20);
+
