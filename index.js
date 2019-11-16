a697b033b270721f837d53c21b6c4ed33eee8de9
Jay Stewart <jayr.stewart@live.com>
2019-11-16 14:06:18 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your code here
+
+let var1 = 31;
+let var2 = 2;
+const multiply = var1 * var2; 
+
+multiply;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let mod = 200 % 7 ; 
+
+const max = 20 ; 
+
+
+
+
